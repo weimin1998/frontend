488380074b1e6cfdfe85dce99f6663ab46967d0d
weimin <2561394933@qq.com>
2023-06-26 17:25:23 +0800

    first commit

---
+++ js/2.string.js
@@ -0,0 +1,11 @@
+let s1 = "string";
+let s2 = 'string';
+let s3 = `string`;
+
+let username = 'weimin';
+let age = 22;
+
+let resurt = `username: ${username}, age: ${age}`;
+
+console.log(resurt);
+
---
+++ js/test.js
@@ -0,0 +1,2 @@
+let a = document.getElementById('p1');
+a.innerText = '33333';
